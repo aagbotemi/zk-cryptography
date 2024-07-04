4339ce7f756855e990157606618d2852d3a5d667
aagbotemi <aabiodunawoyemi@gamil.com>
2024-07-04 23:01:53 +0100

    sumcheck completed

--- sumcheck/src/utils.rs
+++ sumcheck/src/utils.rs
@@ -1,4 +1,5 @@
 use ark_ff::{BigInteger, PrimeField};
+use polynomial::{interface::MLETrait, MLE};
 
 pub fn boolean_hypercube<F: PrimeField>(n: usize) -> Vec<Vec<F>> {
     let mut hypercube = Vec::new();
@@ -20,4 +21,22 @@ pub fn boolean_hypercube<F: PrimeField>(n: usize) -> Vec<Vec<F>> {
 
 pub fn convert_field_to_byte<F: PrimeField>(element: &F) -> Vec<u8> {
     element.into_bigint().to_bytes_be()
+}
+
+pub fn skip_first_and_sum_all<F: PrimeField>(current_poly: MLE<F>) -> MLE<F> {
+    let rounds = current_poly.n_vars - 1;
+    let bh: Vec<Vec<F>> = boolean_hypercube::<F>(rounds);
+
+    let mut bh_sum = MLE::<F>::additive_identity(1);
+
+    for bh_i in bh {
+        let mut sum_except_first = current_poly.clone();
+
+        for bh_i_i in bh_i {
+            sum_except_first = sum_except_first.partial_evaluation(bh_i_i, 1);
+        }
+        bh_sum += sum_except_first;
+    }
+
+    bh_sum
 }
